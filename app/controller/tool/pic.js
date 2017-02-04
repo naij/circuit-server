956ed7bfe5c57c2768ce7ed93590a6f3d27fcbca
naij <naijser@gmail.com>
2017-02-04 17:21:09 +0800

    图片上传更新

--- app/controller/tool/pic.js
+++ app/controller/tool/pic.js
@@ -21,26 +21,27 @@ exports.create = function*() {
   let stream = yield this.getFileStream()
   let filename = stream.filename
   let picName = this.helper.md5(filename) + path.extname(filename)
-  // stream 转换为 buffer
-  let buffer = yield this.helper.streamToBuffer(stream)
-
+  
   try {
+    // stream 转换为 buffer
+    let buffer = yield this.helper.streamToBuffer(stream)
+    
     // 上传到upyun
     yield thunkify(upload)(this.upyunClient, '/c/' + picName, buffer)
+
+    // 获取图片尺寸
+    let size = images(buffer).size()
+
+    // 保存到数据库
+    yield this.service.tool.pic.create({
+      picPath: '/c/' + picName,
+      picSize: size.width + 'x' + size.height
+    })
   } catch(e) {
     this.app.loggers.logger.error('upyun upload error:' + e)
     // 必须将上传的文件流消费掉，要不然浏览器响应会卡死
     yield sendToWormhole(stream)
   }
-
-  // 获取图片尺寸
-  let size = images(buffer).size()
-
-  // 保存到数据库
-  yield this.service.tool.pic.create({
-    picPath: '/c/' + picName,
-    picSize: size.width + 'x' + size.height
-  })
 
   return this.renderJSON({
     code: 200
