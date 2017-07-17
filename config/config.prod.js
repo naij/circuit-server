f27bfaccf89fa57ae3a5266e1c9113ac40f63391
naij <naijser@gmail.com>
2017-07-17 22:20:51 +0800

    up

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,5 +1,5 @@
 exports.host = 'circuit.ueqir.com'
 
-exports.cdn = '//kiwiobjects.b0.upaiyun.com/g/circuit-assets/1.0.0'
+exports.cdn = '//kiwiobjects.b0.upaiyun.com/g/circuit-assets/0.0.1'
 
 exports.keys = 'ueqir'
